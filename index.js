63f369863961836dc48f047b0b9ed7112535c4d6
Sam Plews <the@codeite.com>
2018-02-10 13:35:35 +0000

    simplify

--- index.js
+++ index.js
@@ -9,50 +9,57 @@ const fs = {
 }
 
 fs.mkdir('./data')
-  .then(() => {
-
-    const ws = new WebSocket('wss://ws-feed.gdax.com');
-
-    ws.on('open', function open() {
-      ws.send(JSON.stringify({
-        "type": "subscribe",
-        "product_ids": [
-            "ETH-EUR"
-        ],
-        "channels": [
-            "level2",
-            "matches",
-            "heartbeat",
-            {
-                "name": "ticker",
-                "product_ids": [
-                    "ETH-EUR"
-                ]
-            }
-        ]
-      }));
-    });
-
-    ws.on('message', data => {
-      const message = JSON.parse(data)
-      if (message.time && message.type) {
-        const hourDir = `data/${message.type}`
-        const dir = `data/${message.type}/${message.time.substr(0, 'YYYY-MM-DDThh'.length)}`
-        fs.mkdir(hourDir)
-          .then(() => fs.mkdir(dir))
-          .then(() => fs.writeFile(`${dir}/${message.time}_${message.type}.json`, data))
-          .catch(fatalError)
-      } else {
-        console.log('message:', message)
-      }
-    });
-
-  })
+  .then(() => listen())
   .catch(fatalError)
+
+function listen () {
+  const ws = new WebSocket('wss://ws-feed.gdax.com');
+
+  ws.on('open', function open() {
+    ws.send(JSON.stringify({
+      "type": "subscribe",
+      "product_ids": [
+          "ETH-EUR"
+      ],
+      "channels": [
+          "matches"
+      ]
+    }));
+  });
+
+  ws.on('message', data => {
+    const message = JSON.parse(data)
+    if (message.time && message.type) {
+      if (message.type === 'heartbeat') {
+        console.log('message:', message)
+        return
+      }
+      const hourDir = `data/${message.type}`
+      const dir = `data/${message.type}/${message.time.substr(0, 'YYYY-MM-DDThh'.length)}`
+      fs.mkdir(hourDir)
+        .then(() => fs.mkdir(dir))
+        .then(() => fs.writeFile(`${dir}/${message.time}_${message.type}.json`, data))
+        .catch(fatalError)
+    } else {
+      console.log('message:', message)
+    }
+  });
+
+  ws.on('error', (error) => {
+    console.log('error:: error:', error)
+    listen()
+  })
+
+  ws.on('close', (code, reason) => {
+    console.log('close:: code, reason:', code, reason)
+    listen()
+  })
+
+}
+
 
 function fatalError(err) {
   console.trace(err)
-  process.exit(1)
 }
 
 function mkDirHandler(err) {
